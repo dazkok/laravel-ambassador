b7fcb03c1f1f726a5dd9bb30247d82c7db4c870c
Pavlo Vovk <dazkok@gmail.com>
2023-09-27 20:00:38 +0200

    added orders db, models, controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AmbassadorController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,10 +27,12 @@ Route::prefix('admin')->group(function () {
     Route::middleware(['auth:sanctum', 'scope.admin'])->group(function (){
         Route::get('user', [AuthController::class, 'user']);
         Route::post('logout', [AuthController::class, 'logout']);
-        Route::put('user/info', [AuthController::class, 'updateInfo']);
-        Route::put('user/password', [AuthController::class, 'updatePassword']);
+        Route::put('users/info', [AuthController::class, 'updateInfo']);
+        Route::put('users/password', [AuthController::class, 'updatePassword']);
 
         Route::get('ambassadors', [AmbassadorController::class, 'index']);
+        Route::get('users/{id}/links', [LinkController::class, 'index']);
+        Route::get('orders', [OrderController::class, 'index']);
 
         Route::apiResource('products', ProductController::class);
     });
