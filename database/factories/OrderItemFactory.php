957c8b188836f1d605bc10a06df38452e3d52034
Pavlo Vovk <dazkok@gmail.com>
2023-09-28 11:40:57 +0200

    added orders db, models, controller

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
+ */
+class OrderItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        /** @var Product $product */
+        $product = Product::inRandomOrder()->first();
+        $quantity = fake()->numberBetween(1, 5);
+        return [
+            'product_title' => $product->title,
+            'price' => $product->price,
+            'quantity' => $quantity,
+            'admin_revenue' => 0.1 * $product->price * $quantity
+        ];
+    }
+}
